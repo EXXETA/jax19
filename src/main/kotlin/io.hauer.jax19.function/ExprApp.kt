6e536705333320d7d4294939c78a1f7e6d636de1
Jan Hauer <jan.hauer@exxeta.com>
2019-04-29 15:20:53 +0200

    Simple eval

--- src/main/kotlin/io.hauer.jax19.function/ExprApp.kt
+++ src/main/kotlin/io.hauer.jax19.function/ExprApp.kt
@@ -3,5 +3,10 @@ package io.hauer.jax19.function
 val expression = Div(Const(12), Div(Const(36), Const(9)))
 
 fun main() {
-//    eval(expression).printResult()
+    eval(expression).printResult()
+}
+
+fun eval(expr: Expr): Int = when (expr) {
+    is Const -> expr.value
+    is Div -> eval(expr.left) / eval(expr.right)
 }
