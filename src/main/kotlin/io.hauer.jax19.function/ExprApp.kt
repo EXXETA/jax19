66e2a4d9e3ce14f825f9f58314803f270d84c495
Jan Hauer <jan.hauer@exxeta.com>
2019-04-29 15:28:12 +0200

    Secure eval

--- src/main/kotlin/io.hauer.jax19.function/ExprApp.kt
+++ src/main/kotlin/io.hauer.jax19.function/ExprApp.kt
@@ -1,12 +1,30 @@
 package io.hauer.jax19.function
 
-val expression = Div(Const(12), Div(Const(36), Const(9)))
+import java.util.*
+
+val expression = Div(Const(12), Div(Const(36), Const(90)))
 
 fun main() {
     eval(expression).printResult()
 }
 
-fun eval(expr: Expr): Int = when (expr) {
-    is Const -> expr.value
-    is Div -> eval(expr.left) / eval(expr.right)
+fun eval(expr: Expr): Optional<Int> = when (expr) {
+    is Const -> Optional.of(expr.value)
+    is Div -> {
+        val ergLeft = eval(expr.left)
+        if(ergLeft.isPresent){
+            val ergRight = eval(expr.right)
+            if(ergRight.isPresent){
+                ergLeft.get() div ergRight.get()
+            }
+            else{
+                Optional.empty()
+            }
+        }
+        else{
+            Optional.empty()
+        }
+    }
 }
+
+infix fun Int.div (other : Int) = if(other != 0) Optional.of(this / other) else Optional.empty()
